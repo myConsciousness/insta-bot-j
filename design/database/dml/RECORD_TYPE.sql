5f4f9624f675b15684946e1c07d08e46ef3a1040
Kato Shinya <kato.shinya.dev@gmail.com>
2021-04-10 22:27:44 +0900

    add

---
+++ design/database/dml/RECORD_TYPE.sql
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+INSERT INTO
+  RECORD_TYPE (
+    RECORD_TYPE,
+    RECORD_NAME,
+    CREATED_BY,
+    CREATED_AT,
+    UPDATED_BY,
+    UPDATED_AT
+  )
+VALUES
+  (
+    0,
+    'LIKE',
+    'INSTA_BOT',
+    NOW(),
+    'INSTA_BOT',
+    NOW()
+  );
+INSERT INTO
+  RECORD_TYPE (
+    RECORD_TYPE,
+    RECORD_NAME,
+    CREATED_BY,
+    CREATED_AT,
+    UPDATED_BY,
+    UPDATED_AT
+  )
+VALUES
+  (
+    1,
+    'FOLLOW',
+    'INSTA_BOT',
+    NOW(),
+    'INSTA_BOT',
+    NOW()
+  );
+INSERT INTO
+  RECORD_TYPE (
+    RECORD_TYPE,
+    RECORD_NAME,
+    CREATED_BY,
+    CREATED_AT,
+    UPDATED_BY,
+    UPDATED_AT
+  )
+VALUES
+  (
+    2,
+    'UNFOLLOW',
+    'INSTA_BOT',
+    NOW(),
+    'INSTA_BOT',
+    NOW()
+  );
+INSERT INTO
+  RECORD_TYPE (
+    RECORD_TYPE,
+    RECORD_NAME,
+    CREATED_BY,
+    CREATED_AT,
+    UPDATED_BY,
+    UPDATED_AT
+  )
+VALUES
+  (
+    3,
+    'COMMNET',
+    'INSTA_BOT',
+    NOW(),
+    'INSTA_BOT',
+    NOW()
+  );
