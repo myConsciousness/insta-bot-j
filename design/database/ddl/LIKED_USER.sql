7553fd23d5255b64262458d14fd00e3f2112cf95
Kato Shinya <kato.shinya.dev@gmail.com>
2021-04-10 21:52:45 +0900

    add

---
+++ design/database/ddl/LIKED_USER.sql
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+DROP TABLE IF EXISTS LIKED_USER;
+CREATE TABLE IF NOT EXISTS LIKED_USER(
+  ID INT AUTO_INCREMENT,
+  USER_NAME VARCHAR(30) NOT NULL,
+  PHOTO_URL VARCHAR(1024) NOT NULL,
+  CREATED_BY VARCHAR(50) NOT NULL,
+  CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UPDATED_BY VARCHAR(50) NOT NULL,
+  UPDATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(ID, USER_NAME)
+);
