0991b7193004d7811570210b7433ef8d39d20562
Kato Shinya <kato.shinya.dev@gmail.com>
2021-04-22 23:53:42 +0900

    add

---
+++ app/src/main/java/org/thinkit/bot/instagram/mongo/entity/UserAccount.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.bot.instagram.mongo.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+/**
+ * The entity that manages user account.
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+@Data
+@Document("user_account")
+public final class UserAccount implements Serializable {
+
+    /**
+     * The id
+     */
+    @Id
+    private String id;
+
+    /**
+     * The user name
+     */
+    private String userName;
+
+    /**
+     * The password
+     */
+    private String password;
+}
