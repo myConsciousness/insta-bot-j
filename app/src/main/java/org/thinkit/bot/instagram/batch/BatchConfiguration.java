2c9e4d1a4646986964407dc554637973f0089c42
Kato Shinya <kato.shinya.dev@gmail.com>
2021-04-25 20:28:49 +0900

    update cycle

--- app/src/main/java/org/thinkit/bot/instagram/batch/BatchConfiguration.java
+++ app/src/main/java/org/thinkit/bot/instagram/batch/BatchConfiguration.java
@@ -128,7 +128,7 @@ public class BatchConfiguration {
 
     private boolean logined;
 
-    @Scheduled(cron = "* * * * * *", zone = "Asia/Tokyo")
+    @Scheduled(cron = "* */30 * * * *", zone = "Asia/Tokyo")
     public void performScheduledJob() throws Exception {
 
         JobParameters param = new JobParametersBuilder()
