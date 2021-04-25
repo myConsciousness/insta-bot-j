abe212ba357ec47737b45d93e3a0869ec5259bd9
Kato Shinya <kato.shinya.dev@gmail.com>
2021-04-25 21:08:24 +0900

    update

--- app/src/main/java/org/thinkit/bot/instagram/batch/BatchConfiguration.java
+++ app/src/main/java/org/thinkit/bot/instagram/batch/BatchConfiguration.java
@@ -155,7 +155,7 @@ public class BatchConfiguration {
                 flowBuilder = jobBuilder.flow(this.reversalEntryHashtagStep()).next(this.executeAutolikeStep());
             }
 
-            flowBuilder = flowBuilder.next(this.notifyResultStep());
+            flowBuilder.next(this.notifyResultStep());
         }
 
         return flowBuilder.end().build();
