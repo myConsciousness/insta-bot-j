a1415ee41e3402f8e700226d8eaf6a23c470c41e
Kato Shinya <kato.shinya.dev@gmail.com>
2021-05-08 09:29:39 +0900

    update

--- app/src/main/java/org/thinkit/bot/instagram/batch/strategy/flow/RestrictableBatchFlowStrategy.java
+++ app/src/main/java/org/thinkit/bot/instagram/batch/strategy/flow/RestrictableBatchFlowStrategy.java
@@ -37,7 +37,7 @@ public final class RestrictableBatchFlowStrategy implements BatchFlowStrategy {
                 .next(batchStepCollections.getExecuteAutoLikeStep())
                 .next(batchStepCollections.getUpdateAutoForecastFollowBackUserConfigStep())
                 .next(batchStepCollections.getExecuteAutoForecastFollowBackUserStep())
-                .next(batchStepCollections.getExecuteAutoScrapeUserProfileTasklet())
+                // .next(batchStepCollections.getExecuteAutoScrapeUserProfileTasklet())
                 .next(batchStepCollections.getExecuteAutoFollowStep())
                 .next(batchStepCollections.getExecuteAutoUnfollowStep())
                 .next(batchStepCollections.getNotifyResultStep());
@@ -50,7 +50,7 @@ public final class RestrictableBatchFlowStrategy implements BatchFlowStrategy {
                 .next(batchStepCollections.getExecuteAutoLikeStep())
                 .next(batchStepCollections.getUpdateAutoForecastFollowBackUserConfigStep())
                 .next(batchStepCollections.getExecuteAutoForecastFollowBackUserStep())
-                .next(batchStepCollections.getExecuteAutoScrapeUserProfileTasklet())
+                // .next(batchStepCollections.getExecuteAutoScrapeUserProfileTasklet())
                 .next(batchStepCollections.getExecuteAutoFollowStep())
                 .next(batchStepCollections.getExecuteAutoUnfollowStep())
                 .next(batchStepCollections.getNotifyResultStep());
