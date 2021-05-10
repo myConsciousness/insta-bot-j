eedf05677fdb45c30c83f38537920499fcf5f30b
Kato Shinya <kato.shinya.dev@gmail.com>
2021-05-10 20:45:47 +0900

    update

--- app/src/main/java/org/thinkit/bot/instagram/batch/tasklet/InitializeTasklet.java
+++ app/src/main/java/org/thinkit/bot/instagram/batch/tasklet/InitializeTasklet.java
@@ -18,10 +18,6 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.stereotype.Component;
-import org.thinkit.bot.instagram.batch.data.mongo.repository.FollowBackExpectableUserRepository;
-import org.thinkit.bot.instagram.batch.data.mongo.repository.FollowedUserRepository;
-import org.thinkit.bot.instagram.batch.data.mongo.repository.LikedPhotoRepository;
-import org.thinkit.bot.instagram.batch.data.mongo.repository.MissingUserRepository;
 import org.thinkit.bot.instagram.batch.exception.AvailableUserAccountNotFoundException;
 import org.thinkit.bot.instagram.batch.result.BatchTaskResult;
 import org.thinkit.bot.instagram.catalog.TaskType;
@@ -55,32 +51,6 @@ public final class InitializeTasklet extends AbstractTasklet {
                     There are no available users to run the process.
                     All users are already running or no valid user information has been defined.""");
         }
-
-        final LikedPhotoRepository likedPhotoRepository = super.getMongoCollections().getLikedPhotoRepository();
-        final FollowBackExpectableUserRepository followBackExpectableUserRepository = super.getMongoCollections()
-                .getFollowBackExpectableUserRepository();
-        final FollowedUserRepository followedUserRepository = super.getMongoCollections().getFollowedUserRepository();
-        final MissingUserRepository missingUserRepository = super.getMongoCollections().getMissingUserRepository();
-
-        likedPhotoRepository.findAll().forEach(likedPhoto -> {
-            likedPhoto.setChargeUserName(super.getRunningUserName());
-            likedPhotoRepository.save(likedPhoto);
-        });
-
-        followBackExpectableUserRepository.findAll().forEach(followBackExpectableUser -> {
-            followBackExpectableUser.setChargeUserName(super.getRunningUserName());
-            followBackExpectableUserRepository.save(followBackExpectableUser);
-        });
-
-        followedUserRepository.findAll().forEach(followedUser -> {
-            followedUser.setChargeUserName(super.getRunningUserName());
-            followedUserRepository.save(followedUser);
-        });
-
-        missingUserRepository.findAll().forEach(missingUser -> {
-            missingUser.setChargeUserName(super.getRunningUserName());
-            missingUserRepository.save(missingUser);
-        });
 
         log.debug("END");
         return BatchTaskResult.builder().actionCount(1).build();
