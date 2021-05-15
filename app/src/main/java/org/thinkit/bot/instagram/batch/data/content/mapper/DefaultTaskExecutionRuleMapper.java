3deb7f6166cfb42cd251114e7dd0fd027a9b2aca
Kato Shinya <kato.shinya.dev@gmail.com>
2021-05-15 11:32:28 +0900

    update for the issue (#19)

---
+++ app/src/main/java/org/thinkit/bot/instagram/batch/data/content/mapper/DefaultTaskExecutionRuleMapper.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.bot.instagram.batch.data.content.mapper;
+
+import org.thinkit.bot.instagram.batch.data.content.entity.DefaultTaskExecutionRule;
+import org.thinkit.zenna.mapper.ContentMapper;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * The mapper class that manages the content {@code "DefaultTaskExecutionRule"}
+ * .
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(staticName = "from")
+public final class DefaultTaskExecutionRuleMapper extends ContentMapper<DefaultTaskExecutionRule> {
+
+    /**
+     * The task type code
+     */
+    private int taskTypeCode;
+}
