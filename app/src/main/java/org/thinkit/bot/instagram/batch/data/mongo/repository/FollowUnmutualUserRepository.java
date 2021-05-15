ef01d7c334404c8c908a5d038351afac6beeaaf4
Kato Shinya <kato.shinya.dev@gmail.com>
2021-05-15 21:43:27 +0900

    update for the issue (#27)

---
+++ app/src/main/java/org/thinkit/bot/instagram/batch/data/mongo/repository/FollowUnmutualUserRepository.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.bot.instagram.batch.data.mongo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.thinkit.bot.instagram.batch.data.mongo.entity.FollowUnmutualUser;
+
+/**
+ * The interface that manages follow umutual user repository.
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+@Repository
+public interface FollowUnmutualUserRepository extends MongoRepository<FollowUnmutualUser, String> {
+
+    public List<FollowUnmutualUser> findByChargeUserName(String chargeUserName);
+
+    public FollowUnmutualUser findByUserNameAndChargeUserName(String userName, String chargeUserName);
+}
